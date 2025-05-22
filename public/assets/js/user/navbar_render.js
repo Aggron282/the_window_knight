13d942a47628260523fc91c010302a6fa511f8c6
Aggron282 <marcokhodr1@gmail.com>
2025-05-21 20:56:06 -0700

    S

--- public/assets/js/user/navbar_render.js
+++ public/assets/js/user/navbar_render.js
@@ -32,20 +32,22 @@ const Init_Navbar = () => {
 
     navbar_element.innerHTML = `
     <div class="navbar_custom n-desktop">
+    <div class="banner">Reliable & Affordable Window Cleaning!</div>
 
     <div class="navbar_home">
-<div class="logo-title-container">
-  <img class="logo_new" src="./assets/images/new_logo.png" alt="Logo">
-  <p class="link_nav--title">The Window Knight - Reliable & Affordable Window Cleaning!</p>
-</div>
-<ul class="nav_links">
-  <li><a href="/" class="link_nav">Home</a></li>
-  <li><a href="https://www.quotes.thewindowknight.com" target="_blank" class="link_nav">Window Washing 101</a></li>
-  <li><a href="/about" class="link_nav">About Us</a></li>
-  <li><a href="/contact_us" class="link_nav">Contact Us</a></li>
-  <li class="q-btn"><a href="https://www.quotes.thewindowknight.com" target="_blank" class="link_nav">Free Quote</a></li>
-</ul>
-</div>
+
+      <div class="logo-title-container">
+        <img class="logo_new" src="./assets/images/new_logo.png" alt="Logo">
+        <p class="link_nav--title">The Window Knight</p>
+      </div>
+      <ul class="nav_links">
+        <li><a href="/" class="link_nav">Home</a></li>
+        <li><a href="https://www.quotes.thewindowknight.com" target="_blank" class="link_nav">Window Washing 101</a></li>
+        <li><a href="/about" class="link_nav">About Us</a></li>
+        <li><a href="/contact_us" class="link_nav">Contact Us</a></li>
+        <li class="q-btn"><a href="https://www.quotes.thewindowknight.com" target="_blank" class="link_nav">Free Quote</a></li>
+      </ul>
+      </div>
 
 
     </div>
@@ -109,3 +111,42 @@ const Init_Navbar = () => {
   }
 }
 Init_Navbar();
+
+let scrollTimer;
+let scrollActive = false;
+
+let scrollStartTimer;
+let scrollStopTimer;
+const contactFloat = document.getElementById("contactFloat");
+
+
+
+// window.addEventListener("scroll", () => {
+//   // At top of page, reset everything
+//   if (window.scrollY === 0) {
+//     clearTimeout(scrollStartTimer);
+//     clearTimeout(scrollStopTimer);
+//     contactFloat.classList.remove("scrolling");
+//     scrollActive = false;
+//     return;
+//   }
+//
+//   // Clear stop timer to prevent premature class re-add
+//   clearTimeout(scrollStopTimer);
+//   clearTimeout(scrollStartTimer);
+//   // If scrolling just started
+//   if (!scrollActive) {
+//     scrollStartTimer = setTimeout(() => {
+//
+//     scrollActive = true;
+//     contactFloat.classList.remove("scrolling");
+//   },1000);
+// }else{
+//
+//   // Set a stop timer to re-add class after scroll ends
+//   scrollStopTimer = setTimeout(() => {
+//     contactFloat.classList.add("scrolling");
+//     scrollActive = false;
+//   }, 200);
+// }
+// });
